6185016a7a4d7ed16ec8347ee35e37ccb4f1df64
Jules Weber <jweber@student.42Lyon.fr>
2025-02-11 10:40:54 +0100

    [MOD] modification of merge sort function names

--- src/sorting_algorithm/merge_sort.c
+++ src/sorting_algorithm/merge_sort.c
@@ -13,15 +13,15 @@
 #include "lists_double_circular.h"
 #include "instruction.h"
 
-void	merge_sort_rec(t_stack **stack_array, int *c_pos, int start, int end);
-void	merge_sort_sort(t_stack **stack_array, int *c_pos, int start, int end);
-void	pushing_back(t_stack **stack_array, int start, int end);
-int		get_val1(t_stack b, int c_pos);
-int		get_val2(t_stack b, int c_pos, int middle, int start);
-void	pa_at_middle(t_stack **stack_array, int *c_pos, int middle, int start);
-void	pa_at_start(t_stack **stack_array, int *c_pos);
-void	my_func(t_stack **stack_array, int *pc_pos, int *p_middle, int start);
-int		get_middle(int start, int end);
+static void	merge_sort_rec(t_stack **stacks, int *c_pos, int start, int end);
+static void	merge_sort_sort(t_stack **stacks, int *c_pos, int start, int end);
+static void	pushing_back(t_stack **stacks, int start, int end);
+static void	chose_push(t_stack **stacks, int *pc_pos, int *p_middle, int start);
+int			get_val1(t_stack b, int c_pos);
+int			get_val2(t_stack b, int c_pos, int middle, int start);
+void		pa_at_middle(t_stack **stacks, int *c_pos, int middle, int start);
+void		pa_at_start(t_stack **stacks, int *c_pos);
+int			get_middle(int start, int end);
 
 void	merge_sort(t_stack **stack_array)
 {
@@ -35,45 +35,45 @@ void	merge_sort(t_stack **stack_array)
 	merge_sort_rec(stack_array, &current_pos, start, end);
 }
 
-void	merge_sort_rec(t_stack **stack_array, int *c_pos, int start, int end)
+static void	merge_sort_rec(t_stack **stacks, int *c_pos, int start, int end)
 {
 	int	middle;
 
 	if (end - start <= 1)
 		return ;
 	middle = (start + end) / 2;
-	merge_sort_rec(stack_array, c_pos, start, middle);
-	merge_sort_rec(stack_array, c_pos, middle, end);
-	merge_sort_sort(stack_array, c_pos, start, end);
+	merge_sort_rec(stacks, c_pos, start, middle);
+	merge_sort_rec(stacks, c_pos, middle, end);
+	merge_sort_sort(stacks, c_pos, start, end);
 }
 
-void	merge_sort_sort(t_stack **stack_array, int *c_pos, int start, int end)
+static void	merge_sort_sort(t_stack **stacks, int *c_pos, int start, int end)
 {
 	int	counter;
 
-	while (*c_pos % (stack_array[0]->size) != start)
+	while (*c_pos % (stacks[0]->size) != start)
 	{
 		if (start > *c_pos)
 		{
-			inst_r(stack_array[0]);
+			inst_r(stacks[0]);
 			(*c_pos)++;
 		}
 		else
 		{
-			inst_rr(stack_array[0]);
+			inst_rr(stacks[0]);
 			(*c_pos)--;
 		}
 	}
 	counter = 0;
 	while (start + counter < end)
 	{
-		inst_pb(stack_array[0], stack_array[1]);
+		inst_pb(stacks[0], stacks[1]);
 		counter++;
 	}
-	pushing_back(stack_array, start, end);
+	pushing_back(stacks, start, end);
 }
 
-void	pushing_back(t_stack **stack_array, int start, int end)
+static void	pushing_back(t_stack **stacks, int start, int end)
 {
 	int		i;
 	int		c_pos;
@@ -86,34 +86,34 @@ void	pushing_back(t_stack **stack_array, int start, int end)
 	c_pos = 0;
 	while (i < nb_elems - 1)
 	{
-		c_pos = c_pos % stack_array[1]->size;
+		c_pos = c_pos % stacks[1]->size;
 		if (end <= middle)
 		{
-			pa_at_start(stack_array, &c_pos);
+			pa_at_start(stacks, &c_pos);
 			middle--;
 		}
 		else if (middle <= start)
-			pa_at_middle(stack_array, &c_pos, middle, start);
+			pa_at_middle(stacks, &c_pos, middle, start);
 		else
-			my_func(stack_array, &c_pos, &middle, start);
+			chose_push(stacks, &c_pos, &middle, start);
 		end--;
 		i++;
 	}
-	inst_pa(stack_array[0], stack_array[1]);
+	inst_pa(stacks[0], stacks[1]);
 }
 
-void	my_func(t_stack **stack_array, int *pc_pos, int *p_middle, int start)
+static void	chose_push(t_stack **stacks, int *pc_pos, int *p_middle, int start)
 {
 	int	val1;
 	int	val2;
 
-	val1 = get_val1(*stack_array[1], *pc_pos);
-	val2 = get_val2(*stack_array[1], *pc_pos, *p_middle, start);
+	val1 = get_val1(*stacks[1], *pc_pos);
+	val2 = get_val2(*stacks[1], *pc_pos, *p_middle, start);
 	if (val1 > val2)
 	{
-		pa_at_start(stack_array, pc_pos);
+		pa_at_start(stacks, pc_pos);
 		(*p_middle)--;
 	}
 	else
-		pa_at_middle(stack_array, pc_pos, *p_middle, start);
+		pa_at_middle(stacks, pc_pos, *p_middle, start);
 }
